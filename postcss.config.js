aa4f193e33c0775215c3ae0c99292fa2a108b80a
erikbabcan <erikbabcan@gmail.com>
2025-10-21 15:39:52 +0000

    ok1

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-export default {
+
+module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{svelte,js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./**/*.{html,tsx}"],
   theme: {
-    extend: {
-        fontFamily: {
-          serif: ['-apple-system', 'BlinkMacSystemFont', 'SF Pro Display', 'SF Pro Text', 'Inter', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
-        },
-        colors: {
-          gray: { // Using a neutral gray palette (like zinc) to avoid blue tints
-            50: '#fafafa', 
-            100: '#f4f4f5',
-            200: '#e4e4e7',
-            300: '#d4d4d8',
-            400: '#a1a1aa',
-            500: '#7171a',
-            600: '#52525b',
-            700: '#3f3f46',
-            800: '#2727a',
-            900: '#18181b',
-          }
-        }
-    },
+    extend: {},
   },
   plugins: [],
 }
