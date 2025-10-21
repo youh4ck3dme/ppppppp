144c8719dff3e1e63af1596b5cf4df3a527f59d7
erikbabcan <erikbabcan@gmail.com>
2025-10-21 19:03:17 +0000

    Changes before Firebase Studio auto-run

--- components/Header.tsx
+++ components/Header.tsx
@@ -105,7 +105,7 @@ const Header: React.FC<HeaderProps> = ({ onNavigate, isMenuOpen, setIsMenuOpen }
         <>
         <header
             ref={headerRef}
-            className="absolute top-0 left-0 right-0 z-50"
+            className="absolute top-0 left-0 right-0 z-50 bg-gray-800"
         >
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-20">
